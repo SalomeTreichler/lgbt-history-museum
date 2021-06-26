b6cf8b849c457539797eeb35067d73135296e36d
Nate Widmer <nate.widmer@noseryoung.ch>
2021-06-26 18:03:21 +0200

    added link on button

--- scripts/title.js
+++ scripts/title.js
@@ -1,6 +1,13 @@
+/* The Button needs to be set visible from hidden, so that it doesn't appear at the start
+of the animation */
+gsap.set('#tour-button', {visibility: "visible"})
 gsap.set('#tour-button', {autoAlpha: 0});
 
+/* Animate title in with textAnimation */
+
 gsap.to("#title", {duration: 2, text: "PRIDE HISTORY <br>MUSEUM", delay: 1});
+
+/* Set autoAlpha to 1 to make button appear */
 
 gsap.to('#tour-button', {duration: 2, autoAlpha: 1, delay: 3});
 
