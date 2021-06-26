a4f864b0573b47eaedb42e54768bd454053336a9
Nate Widmer <nate.widmer@noseryoung.ch>
2021-06-26 18:01:48 +0200

    fixed button animation and added hover effect

--- scripts/title.js
+++ scripts/title.js
@@ -1,6 +1,6 @@
-gsap.set('#tourButton', {autoAlpha: 0});
+gsap.set('#tour-button', {autoAlpha: 0});
 
 gsap.to("#title", {duration: 2, text: "PRIDE HISTORY <br>MUSEUM", delay: 1});
 
-gsap.to('#tourButton', {duration: 2, autoAlpha: 1, delay: 3});
+gsap.to('#tour-button', {duration: 2, autoAlpha: 1, delay: 3});
 
