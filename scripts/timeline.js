d56a21231d351f36ff97945aab8dd50e4c009e35
Nate Widmer <nate.widmer@noseryoung.ch>
2021-07-03 17:05:32 +0200

    added more comments to timeline.js and cleaned up code

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,15 +1,18 @@
+/* Function to return html element according to their selector */
 function qs(selector, all = false) {
   return all
     ? document.querySelectorAll(selector)
     : document.querySelector(selector);
 }
 
-const sections = qs(".timeline__section", true);
+/* Define variables from selectors */
+const sections = qs(".timeline-section", true);
 const timeline = qs(".timeline");
-const line = qs(".timeline__line");
+const line = qs(".timeline-line");
 
+/* Define other variables */
 line.style.bottom = `calc(100% - 20px)`;
-let prevScrollY = window.scrollY;
+let prevScrollY = window.scrollY; // returns number of pixels that the document is currently scrolled vertically
 let up, down;
 let full = false;
 let set = 0;
@@ -18,39 +21,42 @@ const targetY = window.innerHeight * 0.8;
 function scrollHandler(e) {
   const { scrollY } = window;
 
+  /* Determine wether the user is scrolling up or down*/
   up = scrollY < prevScrollY;
   down = !up;
 
-  const timelineRect = timeline.getBoundingClientRect();
-  const lineRect = line.getBoundingClientRect();
-  // const lineHeight = lineRect.bottom - lineRect.top;
+  const timelineRect = timeline.getBoundingClientRect(); // returns the smalles rectangle the element can contain
   const dist = targetY - timelineRect.top;
 
   console.log(dist);
 
+  /* Calculates the bottom distance when scrolling down */
   if (down && !full) {
-    set = Math.max(set, dist);
+    set = Math.max(set, dist); // Returns the 'max'imum of the two numbers
     line.style.bottom = `calc(100% - ${set}px)`;
   }
 
+  /* Calculates the bottom distance when scrolling up */
   if (dist > timeline.offsetHeight + 50 && !full) {
     full = true;
     line.style.bottom = `-50px`;
   }
 
+  /* For each section item, calculates if the top and offset height divided by 5 is more than
+  targetY to determine if they are in view. If they are, the show-me class is added */
   sections.forEach((item) => {
-    // console.log(item);
     const rect = item.getBoundingClientRect();
-    //     console.log(rect);
 
     if (rect.top + item.offsetHeight / 5 < targetY) {
       item.classList.add("show-me");
     }
   });
 
-  // console.log(up, down);
+  /* Sets prevScrollY to current scrollY */
   prevScrollY = window.scrollY;
 }
+
+/* Call scrollHandler and add scroll EventListener */
 
 scrollHandler();
 line.style.display = "block";
